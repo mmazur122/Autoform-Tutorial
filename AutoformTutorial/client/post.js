0dd0695f7703891f9f1108640fc5378bed8386b1
Michal Mazur <mmazur@css.edu>
2015-05-27 15:32:49 -0500

    Refactored custom autosave code

--- AutoformTutorial/client/post.js
+++ AutoformTutorial/client/post.js
@@ -13,49 +13,72 @@ Template.post.events({
         console.log("inside form change event: ", event.target);
     },
     'keyup': function(event) {
-        console.log("inside keyup");
-        var arrName = event.target.name + "History";
-        var historyArr = Template.parentData(0)[arrName];
-        if (historyArr) {
+        var _arrName = event.target.name + "History";
+        var _historyArr = Template.parentData(0)[_arrName];
+        if (_historyArr) {
 
-            var lastEditTimeStampMilliseconds = moment(historyArr[historyArr.length-1].date).valueOf();
+            var lastEditTimeStampMilliseconds = moment(_historyArr[_historyArr.length-1].date).valueOf();
             var eventTimeStampMilliseconds = event.timeStamp;
 
             if (eventTimeStampMilliseconds - lastEditTimeStampMilliseconds < 10000) {
-                var lastEdit = historyArr[historyArr.length-1];
-                lastEdit.oldContent = event.target.value;
-                lastEdit.changedBy = Meteor.userId();
-                historyArr[historyArr.length-1] = lastEdit;
+                var _lastEdit = _historyArr[_historyArr.length-1];
+                _lastEdit.oldContent = event.target.value;
+                _lastEdit.changedBy = Meteor.userId();
+                _historyArr[_historyArr.length-1] = _lastEdit;
             } else {
-                console.log("more than 10 seconds passed");
-                var newEdit = {};
-                newEdit.field = historyArr[historyArr.length-1].field;
-                newEdit.changedBy = Meteor.userId();
-                newEdit.oldContent = event.target.value;
-                newEdit.date = new Date(eventTimeStampMilliseconds);
-                //lastEdit.oldContent = event.target.value;
-                historyArr.push(newEdit);
+                var _newEdit = {};
+                _newEdit.field = _historyArr[_historyArr.length-1].field;
+                _newEdit.changedBy = Meteor.userId();
+                _newEdit.oldContent = event.target.value;
+                _newEdit.date = new Date(eventTimeStampMilliseconds);
+
+                _historyArr.push(_newEdit);
             }
 
             var _documentOpts = {
-                _id: Template.parentData(0)._id,
-                field: Template.parentData(0)[arrName][0].field,
-                //field: this.fieldName,
+                _id: _getDocumentId(0),
+                field: _getFieldToUpdate(0, _arrName),
                 newValue: event.target.value,
-                arrName: historyArr
+                arrName: _historyArr,
+                name: _arrName
             };
 
-            var _setObject = {
-                $set: {}
-            };
-
-            _setObject.$set[_documentOpts.field] = _documentOpts.newValue;
-            _setObject.$set[arrName] = _documentOpts.arrName;
-
-            Posts.update({_id: _documentOpts._id}, _setObject);
-            console.log("inside keyup event");
-            console.log("event: ", event, "                ", event.target);
-            //console.log("Parent data: ", Template.parentData(0));
+            updateDocumentIn("Posts", _documentOpts);
         }
     }
-})
+});
+
+function updateDocumentIn(collection, documentOpts) {
+    var _mongoCollection = getMongoCollection(collection);
+
+    if (_mongoCollection) {
+        var _setObject = {
+            $set: {}
+        };
+
+        _setObject.$set[documentOpts.field] = documentOpts.newValue;
+        _setObject.$set[documentOpts.name] = documentOpts.arrName;
+        _mongoCollection.update({_id: documentOpts._id}, _setObject);
+    }
+}
+
+function getMongoCollection(string) {
+    var _mongoCollection = undefined;
+    for (var globalObject in this) {
+        if (this[globalObject] instanceof Meteor.Collection) {
+            if (globalObject === string) {
+                _mongoCollection = (this[globalObject]);
+                break;
+            }
+        }
+    }
+    return _mongoCollection;
+}
+
+function _getDocumentId(number) {
+    return Template.parentData(number)._id;
+}
+
+function _getFieldToUpdate(number, arrName) {
+    return Template.parentData(number)[arrName][0].field;
+}
